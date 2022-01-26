d8005c7559a49c1b6435d6a2d6206acf42cc43e3
jeelpatel17 <jeelpateldev@gmail.com>
2022-01-26 14:37:39 +0530

    made it PWA officially by adding service worker; and made it faster to load by using only local fonts and .css styles; also, added icons for different mobiles

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 import success from "./modules/workoutComplete.js";
 import { loading, fetchData, gymDays, dayName } from "./modules/fetch.js";
 import startWorkout from "./modules/startWorkout.js";
+
+window.onload = () => {
+  "use strict";
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
+  }
+};
+
 doneBtn.addEventListener("click", startWorkout);
 fetchData();
