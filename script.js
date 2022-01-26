1c2c4ff95ab750f9d2045aae97072aaddb78e1c4
jeelpatel17 <jeelpateldev@gmail.com>
2022-01-26 15:05:20 +0530

    renamed sw.js to serviceWorker.js for better readability and changed the font path in css file to its changed path

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = () => {
   "use strict";
 
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./sw.js");
+    navigator.serviceWorker.register("./serviceWorker.js");
   }
 };
 
